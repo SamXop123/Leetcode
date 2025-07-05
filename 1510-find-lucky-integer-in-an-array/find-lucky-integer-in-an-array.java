616eb27abd9d145ed34cb11887e10ff4727b7318
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-07-05 17:16:00 +0530

    Time: 3 ms (77.06%) | Memory: 43.7 MB (15.39%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
@@ -6,15 +6,15 @@ class Solution {
 
         int i = 1;
         int count = 1;
-        int maxLucky = -1;
+        int max = -1;
 
         for (int j = 0; j < arr.length - 1; j++) {
             if (arr[j] == arr[i]) {
                 count++;
             } else {
                 if (arr[j] == count) {
-                    if (arr[j] > maxLucky) {
-                        maxLucky = arr[j];
+                    if (arr[j] > max) {
+                        max = arr[j];
                     }
                 }
                 count = 1;
@@ -23,11 +23,11 @@ class Solution {
         }
 
         if (arr[arr.length - 1] == count) {
-            if (arr[arr.length - 1] > maxLucky) {
-                maxLucky = arr[arr.length - 1];
+            if (arr[arr.length - 1] > max) {
+                max = arr[arr.length - 1];
             }
         }
 
-        return maxLucky;
+        return max;
     }
 }
