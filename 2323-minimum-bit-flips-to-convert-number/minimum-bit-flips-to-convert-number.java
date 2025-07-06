b4332e83159ad0cc2fedeab6a45dbf17c9a50c39
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-07-06 19:58:34 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (23.55%) - LeetSync

---
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,64 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        
+        if (start == goal) {
+            return 0;
+        }
+        
+        int xor = start ^ goal;
+        int steps = 0;
+
+        while (xor != 0) {
+            
+            int lastBit = xor & 1;
+            if (lastBit == 1) {
+                steps++;
+            }
+            xor = xor >> 1;
+        }    
+        return steps;
+    }
+}    
+
+
+/*
+EARLIER APPROACH
+
+public String addZeros(String binaryStr, int targetLength) {
+    while (binaryStr.length() < targetLength) {
+        binaryStr = "0" + binaryStr;
+    }
+    return binaryStr;
+}
+
+public int minBitFlips(int start, int goal) {
+   
+    if(start==goal) {return 0;}
+    
+    int step = 0;
+
+    String binaryStart = Integer.toBinaryString(start);
+    String binaryGoal = Integer.toBinaryString(goal);
+
+    if (binaryStart.length() > binaryGoal.length()) {
+        binaryGoal = addZeros(binaryGoal, binaryStart.length());
+    } else if (binaryGoal.length() > binaryStart.length()) {
+        binaryStart = addZeros(binaryStart, binaryGoal.length());
+    }  
+
+    int num = start&goal;
+    String binary = Integer.toBinaryString(num);
+    binary = addZeros(binary, binaryStart.length());
+
+    for(int i = 0; i < binary.length(); i++) {
+        int bitMask = 1<<i;
+        if((bitMask & num) == 0) {
+            step++;
+        }
+    }
+
+    return step;
+}
+
+LMFAO
+*/
