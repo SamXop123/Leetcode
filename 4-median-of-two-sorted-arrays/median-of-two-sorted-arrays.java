b66e24e70343fcf8b13114fb76da1b36251e5dfa
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-07-16 18:48:08 +0530

    Time: 20 ms (5.19%) | Memory: 46.6 MB (5.57%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -0,0 +1,55 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        int m = nums1.length;
+        int n = nums2.length;
+        int merged[] = new int[n+m];
+        double median;
+
+        int i = 0;
+        int j = 0;
+        int x = 0;
+
+        while(i < m && j < n) {
+            if(nums1[i] > nums2[j]) {
+                merged[x] = nums2[j];
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                merged[x] = nums1[i];
+                i++;
+            } else {
+                merged[x] = nums1[i];
+                i++;
+                x++;
+                merged[x] = nums2[j];
+                j++;
+            }
+            x++;
+        }
+
+        while (i < m) {
+            merged[x] = nums1[i];
+            i++;
+            x++;
+        }
+
+        while (j < n) {
+            merged[x] = nums2[j];
+            j++;
+            x++;
+        }
+
+        if((n+m)%2 == 0) {
+            int a = merged[((n+m) / 2) - 1];
+            int b = merged[(n+m) / 2];
+            median = ((double) a + (double) b) / 2;
+        } 
+        else {
+            median = (double) merged[(n+m) / 2];
+        }
+
+        String formatted = String.format("%.5f", median);
+        return Double.parseDouble(formatted);
+
+    }
+}
