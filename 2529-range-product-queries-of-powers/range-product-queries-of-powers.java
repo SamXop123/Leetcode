af685176b26bea9f04b842c39cf695f1e3cd2548
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-08-11 19:44:00 +0530

    Time: 26 ms (30.11%) | Memory: 93.5 MB (16.13%) - LeetSync

---
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[] productQueries(int n, int[][] queries) {
+        final int MOD = 1_000_000_007;
+        
+        List<Integer> Allpowers = new ArrayList<>();
+
+        for (int i = 1; i <= n; i <<= 1) {
+            Allpowers.add(i);
+        }
+
+        List<Integer> powers = new ArrayList<>();
+        int sum = 0;
+
+        for (int i = Allpowers.size() - 1; i >= 0; i--) {
+            int element = Allpowers.get(i);
+            sum += element;
+
+            if (sum > n) {
+                sum -= element;
+                continue;
+            } else {
+                powers.add(element);
+            }
+
+            if (sum == n) {
+                break;
+            }
+        }
+        Collections.reverse(powers);
+
+        int[] answers = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int left = queries[i][0];
+            int right = queries[i][1];
+            long currPower = 1;
+
+            for (int j = left; j <= right; j++) {
+                currPower = (currPower * powers.get(j)) % MOD;
+            }
+
+            answers[i] = (int) currPower;
+        }
+
+        return answers;
+    }
+}
