a2d06d04df1b3996e53b3b9c1f2fc8b36dc4b8f0
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-09-08 19:45:57 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (14.86%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode inorderSuccessor(TreeNode root) {
+        while(root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null) {
+            return root;
+        }
+
+        if(root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        } 
+        else {
+
+            if(root.left == null && root.right == null) {
+                return null;
+            }
+
+            if(root.right == null) {
+                return root.left;
+            } else if(root.left == null) {
+                return root.right;
+            }
+
+            TreeNode opop = inorderSuccessor(root.right);
+            root.val = opop.val;
+            root.right = deleteNode(root.right, opop.val);
+        }
+        return root;
+    }
+}
