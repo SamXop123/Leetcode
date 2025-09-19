7d905cdb7a62c48e7cf83970c211b04e5199b2e3
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-09-19 19:04:44 +0530

    Time: 21 ms (43.27%) | Memory: 45.3 MB (36.93%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -3,17 +3,18 @@ class Solution {
         int n = s.length();
         if(n <= 1) return s;
             
-        StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
-    
+        StringBuilder sb = new StringBuilder();
+
         for (int center = 0; center < n; center++) {
 
             // ODD
             int left = center;
             int right = center;
-            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
-                if (right - left + 1 > sb1.length()) {
-                    sb1 = new StringBuilder(s.substring(left, right + 1));
+            while (left >= 0 && right < n && 
+            s.charAt(left) == s.charAt(right)) 
+            {
+                if (right - left + 1 > sb.length()) {
+                    sb = new StringBuilder(s.substring(left, right + 1));
                 }
                 left--;
                 right++;
@@ -22,15 +23,17 @@ class Solution {
             // EVEN
             left = center;
             right = center+1;
-            while(left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
-                if (right - left + 1 > sb2.length()) {
-                    sb2 = new StringBuilder(s.substring(left, right + 1));
+            while(left >= 0 && right < n && 
+            s.charAt(left) == s.charAt(right)) 
+            {
+                if (right - left + 1 > sb.length()) {
+                    sb = new StringBuilder(s.substring(left, right + 1));
                 }
                 left--;
                 right++;
             }
         }
 
-        return sb1.length() > sb2.length() ? sb1.toString() : sb2.toString();
+        return sb.toString();
     }
 }
