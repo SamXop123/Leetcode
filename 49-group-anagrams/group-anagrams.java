3f17d5df6a191c852d41ac1c3e07340b1decd338
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-09-19 19:11:58 +0530

    Time: 8 ms (38.34%) | Memory: 48 MB (55.99%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,5 +1,6 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+
         Map<String, List<String>> map = new HashMap<>();
 
         for(String s : strs) {
@@ -10,9 +11,11 @@ class Solution {
             if(!map.containsKey(sorted)) {
                 map.put(sorted, new ArrayList<>());
             }
+
             map.get(sorted).add(s);
         }
-        
+
         return new ArrayList<>(map.values());
+    
     }
 }
